1196bd2555db1709886225aed0efeb5e1b6caeba
riddhiwalekarsod24 <riddhi.walekar.sod24@anu.edu.in>
2025-09-16 12:34:11 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,16 @@
+function setup() {
+  createCanvas(500, 500);
+
+  background('blue');
+  
+  triangle(60, 150, 60, 20, 186, 75);
+triangle(60, 150, 80, 20, 186, 75);
+triangle(60, 150, 100, 20, 186, 75);
+  triangle(60, 150, 120, 20, 186, 75);
+    triangle(60, 150, 140, 20, 186, 75);
+    triangle(60, 150, 160, 20, 186, 75);
+    triangle(60, 150, 180, 20, 186, 75);
+    triangle(60, 150, 200, 20, 186, 75);
+    triangle(60, 150, 220, 20, 186, 75);
+  describe('A white triangle with a black outline on a gray canvas.');
+}
